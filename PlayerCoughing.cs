9d1e653bb28c6e1b5f981a23bf1c0cb85fb5ac26
michu97736 <78175369+michu97736@users.noreply.github.com>
2021-06-17 17:51:22 +0200

    Add files via upload

---
+++ PlayerCoughing.cs
@@ -0,0 +1,39 @@
+﻿using MSCLoader;
+using UnityEngine;
+using HutongGames.PlayMaker;
+using MSCLoader.Helper;
+
+namespace PlayerCoughing
+{
+    public class PlayerCoughing : Mod
+    {
+        public override string ID => "PlayerCoughing";
+        public override string Name => "PlayerCoughing";
+        public override string Author => "michu97736";
+        public override string Version => "1.0";
+        public override string Description => "Adds player coughing after smoking";
+        public FsmFloat cig;
+        public float cigfloat = 0.2f;
+        public Transform Player;
+        public FsmBool cigboolactive;
+        public override void OnLoad()
+        {
+            GameObject ciggameobject = GameObject.Find("PLAYER").transform.Find("Pivot/AnimPivot/Camera/FPSCamera/FPSCamera/Smoking").gameObject;
+            cig = ciggameobject.GetComponent<PlayMakerFSM>().FsmVariables.FindFsmFloat("CigaretteScale");
+            Player = GameObject.Find("PLAYER").transform;
+            cigboolactive = ciggameobject.GetComponent<PlayMakerFSM>().FsmVariables.FindFsmBool("CigaretteBurn");
+
+        }
+
+        public override void Update()
+        {
+            if(cig.Value >= 0 && cigboolactive.Value == true)
+            {
+                if(cInput.GetKeyUp("Smoking"))
+                {
+                    ModHelper.PlaySound3D(Player, "Theatre", "cough1");
+                }
+            }
+        }
+    }
+}
